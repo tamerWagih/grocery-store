6e0a48d35e90e67b246d4f2eb53313ef96bdeca3
Tamer Wagih <mrtamerwagih@gmail.com>
2020-12-18 17:12:38 +0300

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA5QYv9abUML5yAopC1InK1wIwbZ6qcQ84",
+    authDomain: "organic-shop-686c3.firebaseapp.com",
+    projectId: "organic-shop-686c3",
+    storageBucket: "organic-shop-686c3.appspot.com",
+    messagingSenderId: "778556051168",
+    appId: "1:778556051168:web:6a40789f39dc4c75e08a85",
+    measurementId: "G-383Y4VJE93",
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA5QYv9abUML5yAopC1InK1wIwbZ6qcQ84",
+    authDomain: "organic-shop-686c3.firebaseapp.com",
+    projectId: "organic-shop-686c3",
+    storageBucket: "organic-shop-686c3.appspot.com",
+    messagingSenderId: "778556051168",
+    appId: "1:778556051168:web:6a40789f39dc4c75e08a85",
+    measurementId: "G-383Y4VJE93",
+  },
+};
